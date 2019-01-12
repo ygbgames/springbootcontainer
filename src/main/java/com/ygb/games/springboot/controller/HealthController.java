5be19cb1d54e8b6d1a47c1f8825226ad03722879
Yogesh Kumawat <yogeshkumawat@Yogeshs-MacBook-Pro.local>
2019-01-12 13:18:48 -0700

    updating

--- src/main/java/com/ygb/games/springboot/controller/HealthController.java
+++ src/main/java/com/ygb/games/springboot/controller/HealthController.java
@@ -3,12 +3,15 @@ package com.ygb.games.springboot.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+
 @RestController
 public class HealthController {
-
+    private static int counter=0;
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        counter++;
+        return "YGB Games! This is my spring boot container. Request Count : "+counter +" Time : "+new Date();
     }
 
 }
